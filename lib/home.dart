fb9898f61723fa512fb7754255e47090747833f1
anacanestraro <ana.canestraro07@gmail.com>
2025-04-16 09:15:03 -0300

    projeto finalizado

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,8 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
 
     Color fontColor = Colors.white;
+    Color fontColorBlack = Colors.black;
+    Color fontColorBlue = Colors.blue[300]!;
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomePage',),
@@ -21,46 +23,96 @@ class Home extends StatelessWidget {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
               colors: [
-                Colors.black,
-                Colors.pink[300]!,
-                Colors.black,
-                Colors.pink[300]!,
-              ],
-            )
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:<Widget>[
-                Text(
-                  'Lucas Dias - 14',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: fontColor,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'Capitão',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: fontColor,
-                  ),
-                ),
-                Text(
-                  'Alta direita',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: fontColor,
-                  ),
-                ),
+                fontColorBlack,
+                fontColorBlue,
+                fontColorBlack,
+                fontColorBlue,
               ],
             ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                right: 5,
+                top: 0,
+                child: ClipOval(
+                  child: SizedBox(
+                    width: 90,
+                    height: 130,
+                    child: Image.asset(
+                      'images/tartaruga.png',
+                      // fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:<Widget>[
+                    Text(
+                      'Lucas Dias - 14',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: fontColor,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Capitão',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: fontColor,
+                      ),
+                    ),
+                    Text(
+                      'Alta direita',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: fontColor,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone_callback,
+                          color: fontColor,
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          '+55 99999-9999',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: fontColor,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Icon(
+                          Icons.email,
+                          color: fontColor,
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          'dias14@gmail.com',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: fontColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       )
